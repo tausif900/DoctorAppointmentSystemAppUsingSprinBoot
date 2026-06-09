09970a47bdcb6854afb958a34236e920c2bd8b00
tausif900 <ansaritausif9268@gmail.com>
2026-06-09 16:10:35 +0530

    Connectivity Done

---
+++ DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/DoctorAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.study.DoctorAppointmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAppointmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorAppointmentSystemApplication.class, args);
+	}
+
+}
