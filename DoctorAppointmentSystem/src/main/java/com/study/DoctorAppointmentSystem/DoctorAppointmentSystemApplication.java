14178f9bd60e0aba6f5ec33bf93889029b43c4b9
tausif900 <ansaritausif9268@gmail.com>
2026-06-15 21:01:16 +0530

    ModelMapper dependency installed

--- DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/DoctorAppointmentSystemApplication.java
+++ DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/DoctorAppointmentSystemApplication.java
@@ -1,7 +1,9 @@
 package com.study.DoctorAppointmentSystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DoctorAppointmentSystemApplication {
@@ -10,4 +12,8 @@ public class DoctorAppointmentSystemApplication {
 		SpringApplication.run(DoctorAppointmentSystemApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
