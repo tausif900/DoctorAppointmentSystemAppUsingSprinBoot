4f389a64dc71265734fde064726ea26a0cf8efb9
tausif900 <ansaritausif9268@gmail.com>
2026-06-21 20:01:54 +0530

    Doctor crud done

--- DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/controllers/DoctorController.java
+++ DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/controllers/DoctorController.java
@@ -1,9 +1,17 @@
 package com.study.DoctorAppointmentSystem.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +32,40 @@ public class DoctorController {
 	@PostMapping
 	public ResponseEntity<DoctorDto> addDoctor(@RequestBody DoctorDto doctor) {
 		return new ResponseEntity<DoctorDto>(doctorService.addDoctor(doctor), HttpStatus.CREATED);
+	}
+
+//	-----------------------------------------
+//	GET - localhost:8080/doctors
+//	-----------------------------------------
+	@GetMapping
+	public ResponseEntity<List<DoctorDto>> getAllDoctors() {
+		return ResponseEntity.ok(doctorService.getAllDoctors());
+	}
+
+//	-----------------------------------------
+//	GET - localhost:8080/doctors/{id}
+//	-----------------------------------------
+	@GetMapping("/{id}")
+	public ResponseEntity<DoctorDto> getDoctorById(@PathVariable Integer id) {
+		return ResponseEntity.ok(doctorService.getDoctorById(id));
+	}
+
+//	-----------------------------------------
+//	PUT - localhost:8080/doctors/{id}
+//	-----------------------------------------
+	@PutMapping("/{id}")
+	public ResponseEntity<DoctorDto> updateDoctor(@PathVariable Integer id, @RequestBody DoctorDto doctorDto) {
+		return ResponseEntity.ok(doctorService.updateDoctor(id, doctorDto));
+	}
+
+//	-----------------------------------------
+//	DELETE - localhost:8080/doctors/{id}
+//	-----------------------------------------
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Map<String, String>> deleteDoctor(@PathVariable Integer id) {
+		doctorService.deleteDoctor(id);
+		HashMap<String, String> response = new HashMap<String, String>();
+		response.put("message", "Doctor deleted");
+		return ResponseEntity.ok(response);
 	}
 }
--- DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/services/impl/DoctorServiceImpl.java
+++ DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/services/impl/DoctorServiceImpl.java
@@ -1,6 +1,7 @@
 package com.study.DoctorAppointmentSystem.services.impl;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,27 +29,34 @@ public class DoctorServiceImpl implements DoctorService {
 	}
 
 	@Override
+
 	public List<DoctorDto> getAllDoctors() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Doctor> doctor = doctorRepository.findAll();
+		return doctor.stream().map((d) -> modelMapper.map(d, DoctorDto.class)).toList();
 	}
 
 	@Override
 	public DoctorDto getDoctorById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Doctor doctor = doctorRepository.findById(id).orElseThrow(() -> new RuntimeException("Id not found"));
+		return modelMapper.map(doctor, DoctorDto.class);
 	}
 
 	@Override
 	public DoctorDto updateDoctor(Integer id, DoctorDto doctorDto) {
-		// TODO Auto-generated method stub
-		return null;
+		Doctor doctor = doctorRepository.findById(id).orElseThrow(() -> new RuntimeException("Id not found"));
+
+		doctor.setSpecialization(doctorDto.getSpecialization());
+		doctor.setQualification(doctorDto.getQualification());
+		doctor.setExperience(doctorDto.getExperience());
+
+		Doctor updatedDoctor = doctorRepository.save(doctor);
+		return modelMapper.map(updatedDoctor, DoctorDto.class);
 	}
 
 	@Override
 	public void deleteDoctor(Integer id) {
-		// TODO Auto-generated method stub
-
+		Doctor doctor = doctorRepository.findById(id).orElseThrow(()->new RuntimeException("Id not found"));
+		doctorRepository.delete(doctor);
 	}
 
 }
