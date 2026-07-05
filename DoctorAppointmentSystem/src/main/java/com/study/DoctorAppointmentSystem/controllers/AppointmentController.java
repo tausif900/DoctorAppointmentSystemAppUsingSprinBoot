73365257f8df535403ebbc4e4e2d8789978a3170
tausif900 <ansaritausif9268@gmail.com>
2026-07-05 14:14:43 +0530

    methods are still pending of appointments

--- DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/controllers/AppointmentController.java
+++ DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/controllers/AppointmentController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.study.DoctorAppointmentSystem.dtos.AppointmentDto;
 import com.study.DoctorAppointmentSystem.dtos.AppointmentRequestDto;
 import com.study.DoctorAppointmentSystem.dtos.AppointmentResponseDto;
 import com.study.DoctorAppointmentSystem.services.AppointmentService;
