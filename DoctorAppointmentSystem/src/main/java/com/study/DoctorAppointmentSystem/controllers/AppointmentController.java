c0d403a9d5db2db27b6fa050f82416af6c5e7de2
tausif900 <ansaritausif9268@gmail.com>
2026-07-12 22:48:56 +0530

    progress

--- DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/controllers/AppointmentController.java
+++ DoctorAppointmentSystem/src/main/java/com/study/DoctorAppointmentSystem/controllers/AppointmentController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/appointments")
+@CrossOrigin
 public class AppointmentController {
 
 	@Autowired
